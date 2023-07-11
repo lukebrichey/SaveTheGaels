aee7c9bf2c97cdcfff692915e402a8b3c625cdf0
lukebrichey <lukebrichey@gmail.com>
2023-07-11 14:31:58 -0400

    feat: Hidden/draft blogs show for Admin

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -49,7 +49,9 @@ export default function Home() {
               .sort((a, b) => b.num - a.num)
               .map((blog) => {
                 if (blog.hidden) {
-                  return null;
+                  if (!isAdmin) {
+                    return null;
+                  }
                 }
       
                 return (
