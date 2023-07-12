efed6df204eb2d4b301aa513f6b9cae5d1f35475
lukebrichey <lukebrichey@gmail.com>
2023-07-12 10:24:24 -0400

    feat: Updated markdown component to handle code with react-syntax-highlighter

--- frontend/src/components/markdown/Markdown.js
+++ frontend/src/components/markdown/Markdown.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Box, Code, Heading, Text, Link, Image } from '@chakra-ui/react';
-
+import { Box, Heading, Text, Link, Image } from '@chakra-ui/react';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function Markdown({ source }) {
-
+  
   const generateHeading = (size) => ({ node, ...props }) => (
-    <Heading size={size} mt="4" mb="4" {...props} />
+    <Heading size={size} mt="2" mb="4" {...props} />
   );
-    
+
   return (
     <ReactMarkdown
       children={source}
@@ -20,14 +21,22 @@ export default function Markdown({ source }) {
         h5: generateHeading("sm"),
         h6: generateHeading("xs"),
         p: ({node, ...props}) => <Text {...props} />,
-        a: ({node, ...props}) => <Link 
-                                    color="blue.500" 
-                                    textDecoration="underline"
-                                    isExternal
-                                    {...props} 
-                                  />,
+        a: ({node, ...props}) => <Link color="blue.500" {...props} />,
         img: ({node, ...props}) => <Image {...props} />,
-        code: ({node, ...props}) => <Code {...props} />,
+        code: ({node, inline, className, children, ...props}) => {
+          const match = /language-(\w+)/.exec(className || '')
+          return !inline && match 
+            ? <SyntaxHighlighter 
+                style={vscDarkPlus} 
+                language={match[1]} 
+                PreTag="div" {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter> 
+            : <code className={className} {...props}>
+                {children}
+              </code>
+        },
         div: ({node, ...props}) => <Box {...props} />,
       }}
     />
