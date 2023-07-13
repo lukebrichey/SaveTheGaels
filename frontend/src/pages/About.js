adbff848ada2401c7085b2cfb0da8f7a96bc399f
lukebrichey <lukebrichey@gmail.com>
2023-07-13 14:04:44 -0400

    update about

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -19,15 +19,12 @@ export default function About() {
         boxShadow="md"
       >
         <Text fontSize="md" textAlign="center">
-          Hello there! My name is Luke Richey, and I am currently a sophomore
-          at Harvard College studying Computer Science. 
+          Hello there! My name is Luke Richey, and I am currently a junior
+          at Harvard studying Computer Science and Economics. 
           This is my personal blog, dedicated to my late father Brent Richey.
-          I've used the domain name my dad left me that was supposed to be our
-          shared blog. We always talked about writing, but never got around to
-          it. I'll be living out that dream for the both of us. I plan on
-          writing about a lot of different subjects, mostly as an exercise in
-          developing/communicating my ideas as well as forcing myself to dive
-          deeper into subjects I'm interested in. I hope you enjoy! 
+          I plan on writing about a lot of different subjects, mostly as an 
+          exercise in developing/communicating my ideas as well as forcing myself 
+          to dive deeper into subjects I'm interested in. I hope you enjoy! 
         </Text> 
       </Box>
     </Flex>
